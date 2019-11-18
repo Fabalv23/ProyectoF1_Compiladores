e311e8b76ee43539eba665f9551a7a0eb7df21c8
LeslyDubon <35675890+LeslyDubon@users.noreply.github.com>
2019-11-17 19:29:44 -0600

    Agregando Campo_alias

---
+++ minisql/src/minisql/Campo_alias.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minisql;
+
+/**
+ *
+ * @author User
+ */
+public class Campo_alias {
+    private String alias;
+    private String campo;
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+}
